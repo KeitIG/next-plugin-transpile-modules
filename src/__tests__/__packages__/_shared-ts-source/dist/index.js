f3827b0b2154d434981e696ab5bd1bfb138690e6
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-05-12 23:03:51 +0200

    feat: fake dist build

---
+++ src/__tests__/__packages__/_shared-ts-source/dist/index.js
@@ -0,0 +1,5 @@
+// A fake dist folder to be sure that the source is resolved first
+// and not this one
+export const fakeAdd = (a, b) => a + b;
+export const fakeSubstract = (a, b) => a - b;
+export const fakeMultiply = (a, b) => a * b;
